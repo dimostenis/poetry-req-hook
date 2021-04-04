9be69e1030f44f9327d89ff87a2fab17181c078f
Hynek <dimosh3k@gmail.com>
2021-04-04 23:23:32 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyproject-to-requirements-hook",
-    version="0.2.3",
+    version="0.2.4",
     install_requires=["poetry"],
     packages=find_packages(),
     entry_points={
